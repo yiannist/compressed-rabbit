ffb31a919c2ebb23d3e46b345bfc6b596f7c6c6e
Yiannis Tsiouris <tsiou@grnet.gr>
2015-02-10 18:11:15 +0200

    Reformat consumer

--- consumer.py
+++ consumer.py
@@ -4,11 +4,14 @@ import pika
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
+
 channel.queue_declare(queue='archipelago')
 
+print " [*] Waiting for messages. To exit press CTRL+C"
+
 def callback(ch, method, properties, body):
-    print " [x] Received ", ch, method, properties, body
+    print " [x] Received %r" % body
 
 channel.basic_consume(callback, queue='archipelago', no_ack=True)
-print " [*] Waiting for messages. To exit press CTRL+C"
+
 channel.start_consuming()
