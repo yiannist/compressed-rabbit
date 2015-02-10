3caf0f1dc9cd9fc20407ad4ef8f28e2c49ddcc60
Yiannis Tsiouris <gtsiour@softlab.ntua.gr>
2015-02-10 20:50:55 +0200

    Fix C sender for librabbitmq-dev-0.5.2-2

--- rabbitmq.c
+++ rabbitmq.c
@@ -3,31 +3,37 @@
 #include <string.h>
 
 #include <stdint.h>
+#include <amqp_tcp_socket.h>
 #include <amqp.h>
 #include <amqp_framing.h>
 
-#include <unistd.h>
-
+#include "utils.h"
 
 int main(int argc, char **argv)
 {
 
-    int sockfd;
+    int status;
+    amqp_socket_t *socket = NULL;
     amqp_connection_state_t conn;
 
     conn = amqp_new_connection();
 
-    sockfd = amqp_open_socket("localhost", 5672);
-    if (!sockfd) {
-      fprintf(stderr, "opening TCP socket\n");
-      return 1;
+    socket = amqp_tcp_socket_new(conn);
+    if (!socket) {
+        die("creating TCP socket");
     }
-    amqp_set_sockfd(conn, sockfd);
 
-    amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest");
+    status = amqp_socket_open(socket, "localhost", 5672);
+    if (status) {
+        die("opening TCP socket");
+    }
+
+    die_on_amqp_error(amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest"),
+                      "Loggin in");
     amqp_channel_open(conn, 1);
-    amqp_get_rpc_reply(conn);
+    die_on_amqp_error(amqp_get_rpc_reply(conn), "Opening channel");
 
+    /*
     amqp_basic_properties_t props = {
         ._flags = AMQP_BASIC_CONTENT_TYPE_FLAG | AMQP_BASIC_DELIVERY_MODE_FLAG,
         .content_type = amqp_cstring_bytes("text/plain"),
@@ -42,26 +48,25 @@ int main(int argc, char **argv)
                        0,
                        0,
                        AMQP_EMPTY_TABLE);
+    */
 
-    int r = amqp_basic_publish(conn,
-                               1, //chann id
-                               amqp_cstring_bytes(""),
-                               amqp_cstring_bytes("archipelago"),
-                               0,
-                               0,
-                               &props,
-                               amqp_cstring_bytes("cnanakos1")
-                               );
+    die_on_error(amqp_basic_publish(conn,
+                                    1,
+                                    amqp_cstring_bytes(""),
+                                    amqp_cstring_bytes("archipelago"),
+                                    0,
+                                    0,
+                                    NULL,
+                                    amqp_cstring_bytes("cnanakos1")
+                                    ),
+                 "Publishing");
     printf(" [x] Sent 'cnanakos'\n");
 
-    if (r < 0) {
-        fprintf(stderr, "publish error: %s\n", amqp_error_string(-r));
-        exit(1);
-    }
-
-    amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS);
-    amqp_connection_close(conn, AMQP_REPLY_SUCCESS);
-    amqp_destroy_connection(conn);
+    die_on_amqp_error(amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS),
+                      "Closing channel");
+    die_on_amqp_error(amqp_connection_close(conn, AMQP_REPLY_SUCCESS),
+                      "Closing connection");
+    die_on_error(amqp_destroy_connection(conn), "Ending connection");
 
     return 0;
 }
