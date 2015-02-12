d9fd8969d77160ccf647f1b8979c921fd512a683
Yiannis Tsiouris <gtsiour@softlab.ntua.gr>
2015-02-12 14:43:14 +0200

    Minor fix

--- consumer.py
+++ consumer.py
@@ -13,7 +13,7 @@ print " [*] Waiting for messages. To exit press CTRL+C"
 def callback(ch, method, properties, cbody):
     print " --> Received raw data: %r with size %d" % (cbody, len(cbody))
     print " --> Trying to uncompress..."
-    body = lz4.uncompress(str(cbody))
+    body = lz4.uncompress(cbody)
     print " [x] Received '%s' with size %d" % (body, len(body))
 
 channel.basic_consume(callback, queue='archipelago', no_ack=True)
